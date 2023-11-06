fbe58b77691de3f310e73af5076b900a967d1a3d
Arjan Einbu <arjan@einbu.no>
2023-11-06 21:07:47 +0000

    Bugfix to sumOf reducer

--- src/sum.ts
+++ src/sum.ts
@@ -6,9 +6,10 @@ type SummableType = number
 
 export function sumOf<TIn>(extract: SelectorFn<TIn, SummableType>) {
     return (agg: SummableType | NoValue, curr: TIn) => {
+        const extracted = extract(curr)
         return agg === null || agg === undefined
-            ? 0
-            : agg += extract(curr)
+            ? extracted
+            : agg += extracted
     };
 }
 
